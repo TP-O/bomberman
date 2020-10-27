1234b07640e313c8992421307552e69bb341fa44
TP-O <letranphong20122001@gmail.com>
2020-10-27 11:58:01 +0700

    chore: update shell command

--- run.sh
+++ run.sh
@@ -1,1 +1,6 @@
-cd src && find . -type f -name "*.class" -delete && javac App.java && java App && cd -
+find -name "*.java" > sources.txt &&
+find . -type f -name "*.class" -delete &&
+javac @sources.txt &&
+cd src &&
+java App &&
+cd -
