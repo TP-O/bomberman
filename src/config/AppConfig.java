966787797f9f2dcc8b6211a3d01a5a25e36b45a3
wuanganh <quangangtrinh1804@gmail.com>
2020-11-12 11:00:22 +0700

    fix(config): can not read file in Window

--- src/config/AppConfig.java
+++ src/config/AppConfig.java
@@ -4,7 +4,9 @@ public class AppConfig
 {
     public static String Name = "Boom";
 
-    public static String AssetDir = System.getProperty("user.dir") + "/../res";
+    public static String AssetDir = System.getProperty("os.name").equals("Linux")
+        ? System.getProperty("user.dir") + "/../res"
+        : System.getProperty("user.dir") + "/boom/res";
 
     public static String ImageDir = AssetDir + "/image/";
 
