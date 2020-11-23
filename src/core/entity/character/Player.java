e3b2d98093d75c20d7e1ff8b8c3ffd044189390b
TP-O <letranphong20122001@gmail.com>
2020-11-23 11:54:13 +0700

    perf(player): optimize condition of collision detection

--- src/core/entity/character/Player.java
+++ src/core/entity/character/Player.java
@@ -7,8 +7,6 @@ import app.controller.GameController;
 
 public class Player extends Character
 {
-    private int space = 5;
-
     private int margin = 4;
 
     public Player(GameController gameController, float x, float y, int width, int height, int health, int damage, float speed)
@@ -20,33 +18,25 @@ public class Player extends Character
     {
         if (gameController.getKeyService().up.pressed
             && !isCollied(Helper.getXOfTile(x), Helper.getYOfTile(y - margin))
-            && !isCollied(Helper.getXOfTile(x + space), Helper.getYOfTile(y - margin))
             && !isCollied(Helper.getXOfTile(x + width), Helper.getYOfTile(y - margin))
-            && !isCollied(Helper.getXOfTile(x + width - space), Helper.getYOfTile(y - margin))
         ) {
             y -= speed;
         }
         if (gameController.getKeyService().down.pressed
             && !isCollied(Helper.getXOfTile(x), Helper.getYOfTile(y + height + margin))
-            && !isCollied(Helper.getXOfTile(x + space), Helper.getYOfTile(y + height + margin))
             && !isCollied(Helper.getXOfTile(x + width), Helper.getYOfTile(y + height + margin))
-            && !isCollied(Helper.getXOfTile(x + width - space), Helper.getYOfTile(y + height + margin))
         ) {
             y += speed;
         }
         if (gameController.getKeyService().left.pressed
             && !isCollied(Helper.getXOfTile(x - margin), Helper.getYOfTile(y))
-            && !isCollied(Helper.getXOfTile(x - margin), Helper.getYOfTile(y + space))
             && !isCollied(Helper.getXOfTile(x - margin), Helper.getYOfTile(y + height))
-            && !isCollied(Helper.getXOfTile(x - margin), Helper.getYOfTile(y + height - space))
         ) {
             x -= speed;
         }
         if (gameController.getKeyService().right.pressed
             && !isCollied(Helper.getXOfTile(x + width + margin), Helper.getYOfTile(y))
-            && !isCollied(Helper.getXOfTile(x + width + margin), Helper.getYOfTile(y + space))
             && !isCollied(Helper.getXOfTile(x + width + margin), Helper.getYOfTile(y + height))
-            && !isCollied(Helper.getXOfTile(x + width + margin), Helper.getYOfTile(y + height - space))
         ) {
             x += speed;
         }
